ac9fc29bd6b0d0cc390a99df9be5d615fc2c68e3
Nikant <nikant20@gmail.com>
2022-11-14 03:37:26 +0000

    Completed Assignment 3

---
+++ CheckPalindrome.java
@@ -0,0 +1,27 @@
+package Assignment4;
+
+
+//3. WAP to check if “2552” is palindrome or not.
+public class CheckPalindrome {
+	
+	public static boolean checkPalindrome(char[] str) {
+		
+		int j = str.length-1;
+		for(int i = 0;i<j;i++,j--) {
+			if(str[i]!=str[j]) {
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+	
+	public static void main(String[] args) {
+		String input = "2552";
+		boolean checkPalindrome = checkPalindrome(input.toCharArray());
+		System.out.print("Input is Palindrome?  ");
+		System.out.println(checkPalindrome);
+	}
+
+}
