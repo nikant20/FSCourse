1dfd03638acfcfddf16aedcba723cbc740f26177
Nikant <nikant20@gmail.com>
2022-11-14 03:58:46 +0000

    Completed Question 4

---
+++ Assignment4/CountVowelsConsonents.java
@@ -0,0 +1,26 @@
+package Assignment4;
+
+public class CountVowelsConsonents {    
+    public static void main(String[] args) {    
+                
+        int vCount = 0, cCount = 0, sCount = 0;;    
+               
+        String str = "This is a really simple sentence";        
+        str = str.toLowerCase();    
+            
+        for(int i = 0; i < str.length(); i++) {       
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {       
+                vCount++;    
+            }       
+            else if(str.charAt(i) >= 'a' && str.charAt(i)<='z') {         
+                cCount++;    
+            }else {
+            	sCount++;
+            }
+        }    
+        System.out.println("Number of vowels: " + vCount);    
+        System.out.println("Number of consonants: " + cCount);  
+        System.out.println("Number of Special Characters: "+sCount);
+    }    
+}  
+
